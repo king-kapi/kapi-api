2f6f605d202ce716a7cecea5fda7676f5588c735
samuelili <samuelli4521@gmail.com>
2023-09-19 21:21:44 -0700

    Initial commit

---
+++ src/main/kotlin/org/kapi/Application.kt
@@ -0,0 +1,16 @@
+package org.kapi
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.kapi.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
